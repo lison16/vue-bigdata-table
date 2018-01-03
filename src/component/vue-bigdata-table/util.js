33c15664f9e36f1907ccba24d46c528ad5d53bd7
zhigang.li <zhigang.li@tendcloud.com>
2018-01-03 09:48:29 +0800

    replace arguments.callee with findNodeUpper

--- src/component/vue-bigdata-table/util.js
+++ src/component/vue-bigdata-table/util.js
@@ -2,7 +2,7 @@ export const findNodeUpper = (ele, tag) => {
     if (ele.parentNode && ele.parentNode.tagName === tag.toUpperCase()) {
         return ele.parentNode;
     } else {
-        arguments.callee(ele.parentNode, tag);
+        findNodeUpper(ele.parentNode, tag);
     }
 }
 
